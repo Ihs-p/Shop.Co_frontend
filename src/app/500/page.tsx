034437294c96f9a07a47d9301ce0ef001995c4db
ihsan <ihsanp3553@gmail.com>
2025-01-12 13:16:32 +0530

    fixed stuff

--- src/app/500/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Custom500() {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <h1 className="text-4xl font-bold">500 - Server-side error occurred</h1>
-      </div>
-    );
-  }
-  
